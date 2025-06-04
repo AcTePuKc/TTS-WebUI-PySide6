1007ba6c85a1b63e7373808ceb51f7a825957120
Shteryan Nikolaev <actepukc@gmail.com>
2025-06-04 16:04:26 +0100

    Install optional backends in active env

--- gui_pyside6/utils/install_utils.py
+++ gui_pyside6/utils/install_utils.py
@@ -2,21 +2,11 @@ from __future__ import annotations
 
 import subprocess
 import sys
-from pathlib import Path
 from typing import Iterable
 
 
-VENV_DIR = Path.home() / ".hybrid_tts" / "venv"
-
-
-def _ensure_venv():
-    if not VENV_DIR.exists():
-        subprocess.check_call([sys.executable, "-m", "venv", str(VENV_DIR)])
-
-
 def install_package_in_venv(package: str | Iterable[str]):
-    _ensure_venv()
+    """Install packages into the environment running this process."""
     if isinstance(package, str):
         package = [package]
-    pip = VENV_DIR / "bin" / "pip"
-    subprocess.check_call([str(pip), "install", *package])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", *package])
